a79f7a29dd0bb99095895dcc195c1962c2e83835
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-10-02 17:09:10 -0400

    Moved textarea ids into parent div tags

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,6 +6,14 @@ $(document).ready(function () {
     var displayDate = $('#currentDay');
     displayDate.text(currentDate);
 
-
+    //Save Events to Local Storage using "this"
+    $('.saveBtn').click(function() {
+    $(this).value; 
+     localStorage.setItem('save9', hr9);
+    /*var hour9 = document.getElementById('hr9').value;
+    localStorage.setItem('save9', hour9);
+    }); */
 
 });
+
+// Need to display past, current or future below with a for loop 
