82440347e9c4e3244364d72e79366fdac37415a3
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-10-03 11:26:58 -0400

    Changed rows to default class of future and changed clock display on script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,32 @@
 $(document).ready(function () {
     // Variable that displays the date in shorthand Day then Month, Date, Year & Time 
-    var currentDate = moment().format('llll');
+    var currentDate = moment().format('dddd, MMMM Do');
 
     // Display current day
-    var displayDate = $('#currentDay');
-    displayDate.text(currentDate);
+   $('#date-display').text(currentDate);
+
+   var currentHour = moment().format('H'); // gives 24 hr clock
+
+   if (currentHour >= 9) {
+    for (i = 0; i <= currentHour - 9; i++) {
+        if (currentHour - 9 === i) {
+            $('#' + i).attr('class', 'present');
+        } else {
+            $('#' + i).attr('class', 'past');
+        }
+    }
+   }
 
     //Save Events to Local Storage using "this"
-    $('.saveBtn').click(function() {
-    $(this).value; 
-     localStorage.setItem('save9', hr9);
+    /*$('.saveBtn').click(function() {
+    /$(this).parent('div')/*$("button:nth-of-type").value; 
+    /*localStorage.setItem('save9', hr9);
     /*var hour9 = document.getElementById('hr9').value;
     localStorage.setItem('save9', hour9);
     }); */
 
 });
 
+
 // Need to display past, current or future below with a for loop 
+//start loop after 9 am, 
